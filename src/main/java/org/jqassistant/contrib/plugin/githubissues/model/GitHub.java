b6cf97a81a3a17fec5dea12126818849675294c6
Stephan Pirnbaum <stephan.pirnbaum@googlemail.com>
2023-03-30 16:55:36 +0200

    Rework plugin

--- src/main/java/org/jqassistant/contrib/plugin/githubissues/model/GitHub.java
+++
@@ -1,9 +0,0 @@
-package org.jqassistant.contrib.plugin.githubissues.model;
-
-import com.buschmais.jqassistant.core.store.api.model.Descriptor;
-import com.buschmais.xo.neo4j.api.annotation.Label;
-
-@Label("GitHub")
-public interface GitHub extends Descriptor {
-
-}
